fc3938610f20f7b123a45c85e568424a61bb2816
sidbhargava1 <sid.bhargava001@gmail.com>
2015-09-09 16:27:46 -0400

    adds all routing updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
+require 'api_constraints'
+
 CheqApi::Application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/' do
+  scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+
+  end
     #resources here
   end
 end
