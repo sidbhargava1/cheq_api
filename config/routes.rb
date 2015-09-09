f4d4f348fe82f3d31750b5f1ef1b5b200a8bb336
sidbhargava1 <sid.bhargava001@gmail.com>
2015-09-09 13:45:14 -0400

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
-Rails.application.routes.draw do
-
+CheqApi::Application.routes.draw do
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/' do
+    #resources here
+  end
 end
